2635f5a93c6b7f91e0654b1ef77949e32431b85e
reistence <gvlamberti@gmail.com>
2022-10-27 14:45:01 +0200

    add, remove, done/not, add whit enter implemented

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      tasks: [
+        {
+          text: "Clean Room",
+          done: false,
+        },
+        {
+          text: "Do homework",
+          done: true,
+        },
+        {
+          text: "Clean Room",
+          done: false,
+        },
+        {
+          text: "Do homework",
+          done: true,
+        },
+        {
+          text: "Clean Room",
+          done: false,
+        },
+        {
+          text: "Do homework",
+          done: true,
+        },
+      ],
+      newTask: {
+        text: "",
+        done: false,
+      },
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.text) {
+        this.newTask.text.trim();
+        this.tasks.push({ ...this.newTask });
+        this.newTask.text = "";
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    doTask(index) {
+      if (this.tasks[index].done === true) {
+        this.tasks[index].done = false;
+      } else {
+        this.tasks[index].done = true;
+      }
+    },
+  },
+}).mount("#app");
