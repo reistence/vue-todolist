5fc8215129dab41a4aebb59cf9dfd7e4b4d9bfc6
reistence <gvlamberti@gmail.com>
2022-10-27 15:47:55 +0200

    error msgs implemented in addTask

--- js/script.js
+++ js/script.js
@@ -4,51 +4,61 @@ createApp({
   data() {
     return {
       tasks: [
-        {
-          text: "Clean Room",
-          done: false,
-        },
-        {
-          text: "Do homework",
-          done: true,
-        },
-        {
-          text: "Clean Room",
-          done: false,
-        },
-        {
-          text: "Do homework",
-          done: true,
-        },
-        {
-          text: "Clean Room",
-          done: false,
-        },
-        {
-          text: "Do homework",
-          done: true,
-        },
+        // {
+        //   text: "Clean Room",
+        //   done: false,
+        // },
+        // {
+        //   text: "Do homework",
+        //   done: true,
+        // },
+        // {
+        //   text: "Clean Room",
+        //   done: false,
+        // },
+        // {
+        //   text: "Do homework",
+        //   done: true,
+        // },
+        // {
+        //   text: "Clean Room",
+        //   done: false,
+        // },
+        // {
+        //   text: "Do homework",
+        //   done: true,
+        // },
       ],
       newTask: {
         text: "",
         done: false,
       },
+      longEnough: true,
+      empty: false,
     };
   },
   methods: {
     addTask() {
-      if (this.newTask.text) {
-        this.newTask.text.trim();
-        this.tasks.push({ ...this.newTask });
-        this.newTask.text = "";
+      if (this.newTask.text != "") {
+        this.empty = false;
+        if (this.newTask.text.length <= 1) {
+          this.longEnough = false;
+        } else {
+          this.longEnough = true;
+          this.newTask.text.trim();
+          this.tasks.push({ ...this.newTask });
+          this.newTask.text = "";
+        }
+      } else {
+        this.empty = true;
       }
     },
     removeTask(index) {
       this.tasks.splice(index, 1);
     },
-    removeLastTask() {
-      this.tasks.pop();
-    },
+    // removeLastTask() {
+    //   this.tasks.pop();
+    // },
     doTask(index) {
       if (this.tasks[index].done === true) {
         this.tasks[index].done = false;
